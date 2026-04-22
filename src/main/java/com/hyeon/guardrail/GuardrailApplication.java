0851baef6ae815c40a6fbb672c578f99ca597e92
HyeonProG <guswo3443@naver.com>
2026-04-22 21:30:13 +0900

    first commit

---
+++ src/main/java/com/hyeon/guardrail/GuardrailApplication.java
@@ -0,0 +1,13 @@
+package com.hyeon.guardrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardrailApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GuardrailApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/hyeon/guardrail/GuardrailApplicationTests.java
@@ -0,0 +1,11 @@
+package com.hyeon.guardrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardrailApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
